b4f9cce5e12daf77095456078b9f2db4e5844af0
hp-zhaocj <71261645+hp-zhaocj@users.noreply.github.com>
2020-09-18 09:57:58 +0800

    Initial commit

---
+++ student_.py
@@ -0,0 +1,21 @@
+import fire
+import os
+import sys
+import importlib
+class A():
+    def hello(self,name):
+        print("你好呀")
+        return "hello"+name
+    #
+    def student(self):
+        print("student")
+        return "helllo"
+    @property
+    def print_(self):
+        print("xz"+os.getcwd())
+        a = importlib.import_module("test_scr.s1")
+        a.main()
+        return "dfdfdfds"
+
+if __name__ == '__main__':
+    fire.Fire(A)
